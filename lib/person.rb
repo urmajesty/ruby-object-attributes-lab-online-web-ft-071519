96a5db484945c91c5e7d0d18b75e7c1e2ce21f1e
Chyrelle Lewis <chyrellelewis@gmail.com>
2019-08-02 17:03:29 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,2 +1,14 @@
 class Person
+  def name = (name)
+    @name = name
+  end
+  def job = (job)
+    @job = job
+  end
   
+  lady = Person.new
+  lady.name = "Lady"
+  job.name = "Jobby"
+  
+  lady.name
+  job.name
