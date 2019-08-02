5d2011e884b5cab0466008446160de869c078265
Chyrelle Lewis <chyrellelewis@gmail.com>
2019-08-02 12:18:32 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,21 @@
+
+  class Dog
+  def initialize(name, breed)
+  @name = name
+  @breed = breed
+  end
+
+
+def name
+  @name
+
+end
+
+def breed
+  @breed
+end
+end
+
+dog = Dog.new("Fido")
+dog.name
+dog.breed
