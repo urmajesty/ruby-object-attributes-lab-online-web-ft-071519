e5cdc7a8fa555ea55bf9b41a86daa9cc911699d2
Chyrelle Lewis <chyrellelewis@gmail.com>
2019-08-02 16:46:29 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,8 +5,18 @@ class Dog
   def  name
     @name
   end
+def breed
+  @breed = breed
+end
+
+def breed
+  @breed
+end
 end
     lassie = Dog.new
     lassie.name = "Lassie"
+     lassie.breed = "Golden Retriever"
      
+     lassie.name
+     lassie.breed
   
