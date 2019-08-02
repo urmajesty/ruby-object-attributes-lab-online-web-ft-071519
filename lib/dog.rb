bfa77ea83b7bdd04e638edf7d65d1c2e311a31f9
Chyrelle Lewis <chyrellelewis@gmail.com>
2019-08-02 13:36:56 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,19 +1,14 @@
 
   class Dog
-  def initialize(name, breed)
-    @name= name
-    @breed= breed
-
+Dog.new
+end
 
 def name
-  @name
+  @name = name
 end
 
-def breed
-  @breed
-end
-end
-end
+def @
+
 
 dog = Dog.new("dog")
 dog.name
--- lib/person.rb
+++ lib/person.rb
@@ -1,8 +1,1 @@
-class Person
-  def initialize(name)
-    @name = name
-  end
-  
-  def name
-    @name
-  end
+
